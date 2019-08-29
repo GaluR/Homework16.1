b61ea66a9fd78b7be325d960e71e8d33f98afc9c
Bartosz Galinski <galinski.bartosz@gmail.com>
2019-08-29 10:48:09 +0200

    Change methods in main class.

--- src/Competition.java
+++ src/Competition.java
@@ -8,26 +8,19 @@ public class Competition {
     private static String STOP = "stop";
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+
         File file = new File("stats.csv");
-        try {
-            file.createNewFile();
-            List<Participant> participants = new ArrayList<>();
-            FileWriter fw = new FileWriter(file);
-            BufferedWriter bfw = new BufferedWriter(fw);
-            loadWriteAndSort(input, participants, bfw);
-            System.out.println("Dane posortowane i zapisano do pliku stats.csv .");
-        } catch (IOException ex) {
-            System.out.println("Nie udało się odnaleźć lub utworzyć pliku.");
-        }
+        write(file, load());
+        System.out.println("Dane posortowane i zapisano do pliku stats.csv .");
     }
 
-    private static void loadWriteAndSort(Scanner input, List<Participant> participants, BufferedWriter bfw) {
+    private static List<Participant> load() {
+        List<Participant> participants = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
         int result = 0;
         String name = null;
-        try {
-            do {
-
+        do {
+            try {
                 System.out.println("Podaj imię następnego gracza lub wpisz komendę STOP w celu zakończenia działania programu.");
                 name = input.nextLine();
                 if (name.equals(STOP)) {
@@ -37,8 +30,17 @@ public class Competition {
                     input.nextLine();
                     participants.add(new Participant(name, result));
                 }
+            } catch (InputMismatchException ex) {
+                System.out.println("Wynik musi być wyrażony liczbowo");
+            }
+        } while (name != STOP);
+        return participants;
+    }
 
-            } while (name != STOP);
+    private static void write(File file, List<Participant> participants) {
+        try {
+            FileWriter fw = new FileWriter(file);
+            BufferedWriter bfw = new BufferedWriter(fw);
             Collections.sort(participants);
             bfw.write(participants.toString());
             bfw.flush();
